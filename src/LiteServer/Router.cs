a48324ba33cc0522676f586879bad2ada1bb5c37
Adewole <wolecharles@gmail.com>
2024-07-19 18:12:21 +0100

    class for construction

---
+++ src/LiteServer/Router.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Net.Http;
+using System.Net;
+
+namespace LiteServer
+{
+    public class RouterManager
+    {
+        private readonly RouteTable routeTable;
+
+        public RouterManager()
+        {
+        }
+    }
+
+    public class RouteTable
+    {
+        private readonly ConcurrentDictionary<(string, string), ActionMethod> _table = new ConcurrentDictionary<(string, string), ActionMethod>();
+
+        public RouteTable()
+        {
+        }
+
+        public ActionMethod GetActionMethod((string,string) key)
+        {
+            if (_table.TryGetValue(key, out var actionMethod)){
+                return actionMethod;
+            }
+            throw new KeyNotFoundException();
+        }
+    }
+
+    public class ActionMethod
+    {
+        private Uri _uri;
+        private HttpMethod httpMethod;
+        private string _actionName;
+        private string _controllerName;
+        private object responseValue;
+        private Type responseType;
+        private Func<object[], object> responseFunc;
+        private IList<MethodParameter> parameterCollection;
+        private IList<Attribute> actionAttribute;
+    }
+
+    public class MethodParameter
+    {
+        private object value { get; set; }
+
+        private Type type { get; set; }
+
+        private int order { get; set; }
+    }
+
+    public class ControllerContext
+    {
+        private HttpListenerRequest _request;
+        private HttpListenerResponse _response;
+        private Uri _uri;
+        private ModelState _modelState;
+    }
+
+    public class ModelState
+    {
+
+    } 
+
+}
